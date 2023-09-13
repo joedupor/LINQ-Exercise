e21b0fb22951579a6cc11aba2e57660bc7bc9785
joedupor <joedupor@gmail.com>
2023-09-13 14:14:33 -0500

    init commit

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace LINQ
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> colors = new List<string> () {"blue", "green", "red", "yellow", "orange", "purple", "brown", "gray", "black" };
+            
+            //Order the list of colors by length of the color name using LINQ
+
+            var lengthOfColorName = colors.OrderBy (c => c.Length);    //where c is a string in the list and .Length gives the length of that string
+            
+            foreach (var item in lengthOfColorName) 
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
